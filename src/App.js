d0a73f107bb972d97b7b2f372316e97e2fa572b7
Loc Tran <mrlocspoc@gmail.com>
2022-12-02 21:33:26 +1030

    314. Styled Components - Basics complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import Testing from "./Testing";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+	return (
+		<div>
+			<h4>comfy sloth starter</h4>
+			<Testing />
+		</div>
+	);
 }
 
-export default App
+export default App;
