31ef2efc5de9c67cf7b46fbc346083890d0d6fe7
Loc Tran <mrlocspoc@gmail.com>
2023-01-21 16:31:04 +1030

    396. AddToCart - Setup complete

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context';
 import AmountButtons from './AmountButtons';
 
 const AddToCart = ({ product }) => {
+	const { addToCart } = useCartContext();
 	const { id, stock, colors } = product;
 	// console.log('colors: ', colors);
 	const [mainColor, setMainColor] = useState(colors[0]);
@@ -60,7 +61,11 @@ const AddToCart = ({ product }) => {
 					increase={increase}
 					decrease={decrease}
 				/>
-				<Link to="/cart" className="btn">
+				<Link
+					to="/cart"
+					className="btn"
+					onClick={() => (id, mainColor, amount, product)}
+				>
 					add to cart
 				</Link>
 			</div>
